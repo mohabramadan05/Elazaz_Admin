7f6a9b2e7839a151ef5c4ad66844ecc57e15bbaf
mohabramadan05 <mohab.ramadan05@gmail.com>
2026-01-09 22:53:50 +0100

    9 JAN 03

--- components/dashboard/app-sidebar.tsx
+++ components/dashboard/app-sidebar.tsx
@@ -167,7 +167,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
 
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser />
       </SidebarFooter>
 
       <SidebarRail />
